a1dc6cfab8d2b5bf724bf4a08793b664ca0b81d8
yangzhangs <yangzhang15@nudt.edu.cn>
2018-07-27 13:32:30 +0800

    travel

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,40 @@
 var addressPoints = [
   [
+    "Beijing, China",
+    39.8591547930,
+    116.4550781250
+  ],
+  [
+    "Wuhan, Hubei, China",
+    30.5717205652,
+    114.3127441406
+  ],
+  [
+    "Chengdu, Sicuan, China",
+    30.5433389542,
+    104.0405273438
+  ],
+  [
+    "Nanjing, Jiangsu, China",
+    31.9521622380,
+    118.8061523438
+  ],
+  [
+    "Quzhou, Zhejiang, China",
+    28.9504756748,
+    118.8665771484
+  ],
+  [
+    "Shanghai, China",
+    31.2221970321,
+    121.5087890625
+  ],
+  [
+    "Sanya, Hainan, China",
+    18.2293513384,
+    109.5007324219
+  ],
+  [
     "Changsha, Hunan, China",
     28.2281638262,
     112.9806218717
@@ -10,9 +45,44 @@ var addressPoints = [
     -121.7381286621
   ],
   [
+    "Sacramento, CA, USA",
+    38.5771583773,
+    -121.4888763428
+  ],
+  [
+    "Berkeley, CA, USA",
+    37.8531699589,
+    -122.2723388672
+  ],
+  [
+    "South Lake Tahoe, CA, USA",
+    38.9423209795,
+    -119.9707031250
+  ],
+  [
+    "Yosemite National Park, CA, USA",
+    37.8661807853,
+    -119.5422363281
+  ],
+  [
+    "Salt Lake City, UT, USA",
+    40.7597405921,
+    -111.8902587891
+  ],
+  [
+    "Yellowstone National Park, WY, USA",
+    44.4337798461,
+    -110.5883789063
+  ],
+  [
     "San Francisco, CA, USA",
     37.7792808,
     -122.4192362
+  ],
+  [
+    "Las Vegas, NV, USA",
+    36.1290016557,
+    -115.1147460938
   ],
   [
     "Los Angeles, CA, USA",
